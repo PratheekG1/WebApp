3f153924aa4847455c24acf31a9d2333048a5096
Pratheek G <89034352+PratheekG20@users.noreply.github.com>
2023-06-22 20:05:43 +0530

    Revert "commit02 on branch2"

--- WebApp/src/AppStructure/test01.java
+++ WebApp/src/AppStructure/test01.java
@@ -3,8 +3,6 @@ package AppStructure;
 public class test01{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test 02");
-		System.out.println("Test 02");
-
+		System.out.println("Test 01");
 	}
 }
