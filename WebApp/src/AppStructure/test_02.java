9b11d816ff3316cae18719de315367b94fa36cfa
User <User@DESKTOP-IOLE50H>
2023-06-22 19:07:43 +0530

    updated

--- WebApp/src/AppStructure/test_02.java
+++ WebApp/src/AppStructure/test_02.java
@@ -5,7 +5,7 @@ public class test_02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test 02");
-
+		System.out.println("TAdded comment after master branch mergee");
 	}
 
 }
