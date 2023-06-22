71af5a8c7a3b13bd63c1eb22d96a349cad88686d
User <User@DESKTOP-IOLE50H>
2023-06-22 19:02:13 +0530

    commit02 on branch2

--- WebApp/src/AppStructure/test01.java
+++ WebApp/src/AppStructure/test01.java
@@ -1,5 +1,10 @@
 package AppStructure;
 
 public class test01{
-	ad
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test 02");
+		System.out.println("Test 02");
+
+	}
 }
