46f6b6a2cb0f568cfbe043e4e951a7b86309cc82
User <User@DESKTOP-IOLE50H>
2023-06-22 19:01:10 +0530

    commit1

--- WebApp/src/AppStructure/test_02.java
+++ WebApp/src/AppStructure/test_02.java
@@ -1,8 +1,11 @@
 package AppStructure;
 
-public class test01{
+public class test_02 {
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test 01");
+		System.out.println("Test 02");
+
 	}
+
 }
